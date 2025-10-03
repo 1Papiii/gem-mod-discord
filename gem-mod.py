f9c574d05fac53099e4d68ce84132ddb7fab7289
1Papiii <1papinoseconds@gmail.com>
2025-10-03 10:08:03 -0500

    Update gem-mod.py
    Added proper Gemini implementation

--- gem-mod.py
+++ gem-mod.py
@@ -1,18 +1,18 @@
 from google import genai
 from google.genai import types
-from discordwebhook import Discord as hook
+from discord.ext import commands
+from discord.ext.commands import Bot
+import time
+import discord
 import random
 
-# API key randomizer (bc im broke) and chat setup
-# No keys are provided in this repo
-# NEVER share your API key with ANYONE
-keys = ["", "", "", "", "", "", "", "", "", ""]
+keys = ["", "", "", "", "", ""]
 api_key = random(keys)
-
-# Bot setup type shiiii
-# This bot doesn't use any command prefixes due to technical limitations with the GenAI SDK
+#Bot setup type shiiii
+#This bot doesn't use any command prefixes due to technical limitations with the GenAI SDK
 intents = discord.Intents.default()
 intents.message_content = True
+gem_client = genai.Client()
 client = discord.Client(intents=intents)
 bot = commands.Bot(intents=intents)
 
@@ -23,10 +23,19 @@ bot = commands.Bot(intents=intents)
 async def on_message(message):
     if message.author == client:
         return
+    elif message.author == 1264014388021559491:
+        return
     else:
         await bot.process_commands(message)
         print(message.content)
+        response = gem_client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction="You, as an AI and moderator of a school Discord server, are to classify each message sent as `NEGATIVE`, `POSITIVE`. `NEGATIVE` content includes words such as 'slime', 'fuh', 'goon' and any other derogatory/offensive slang."
+            ),
+            contents=message.content
+        )
+        
+        print(response.text)
 
-# The blank space is your token
-# NEVER leave your token out kids
 bot.run('')
