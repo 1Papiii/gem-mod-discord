d8d7b4fd26b9a725e7932709356cdbdc1a7aa3d8
1Papiii <1papinoseconds@gmail.com>
2025-10-04 23:14:17 -0500

    Update gem-mod.py
    Fixed a bug on macOS

--- gem-mod.py
+++ gem-mod.py
@@ -18,28 +18,30 @@ gem_client = genai.Client()
 client = discord.Client(intents=intents)
 bot = commands.Bot(intents=intents)
 
-#Honestly idk what any of this does
-#I think it just collects a message and blah blah blah all that shit
-#Blame dev.to for the horrid code :(
+# Honestly idk what any of this does
+# I think it just collects a message and blah blah blah all that shit
+# Blame dev.to for the horrid code :(
 @bot.event
 async def on_message(message):
+    prefix_fix(bot, message)
+    await bot.process_commands(message)
+    print(message.content)
     if message.author == client.user:
         return
     else:
-        await bot.process_commands(message)
-        print(message.content)
         response = gem_client.models.generate_content(
             model="gemini-2.5-flash",
             config=types.GenerateContentConfig(
-                system_instruction="You, as an AI and moderator of a school Discord server, are to classify each message sent as NEGATIVE, POSITIVE. NEGATIVE content includes words such as 'slime', 'fuh', 'goon' and any other derogatory/offensive slang. However, simple swear words such as 'fuck' or 'shit' are allowed by technicality. Those words are POSITIVE in this case and should not be marked as NEGATIVE."
-            ),
+                system_instruction="You, as an AI and moderator of a school Discord server, are to classify each message sent as NEGATIVE, POSITIVE. NEGATIVE content includes words such as 'slime', 'fuh', 'goon' and any other derogatory/offensive slang. However, simple swear words such as 'fuck' or 'shit' are allowed by technicality. Those words are POSITIVE in this case and should not be marked as NEGATIVE. If there are any links sent, treat them as 'NULL' and respond with NULL."
+                ),
             contents=message.content
-        )
+            )
 
         print(response.text)
         if response.text == "NEGATIVE":
-            await message.delete()
-        elif response.text == "POSITIVE":
-            return
+        await message.delete()
+        await message.channel.send("Message deleted. Inappropriate content detected.")
+    elif response.text == "POSITIVE":
+        return
 
-bot.run('BOT_TOKEN_HERE')
+bot.run('')
